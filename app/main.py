80adbeca72b127ccecb94292e2fd1da24774509e
pranjalpokharel7 <pranjalpokharel007@gmail.com>
2025-07-13 14:33:01 +0545

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,12 +1,32 @@
 import socket  # noqa: F401
 
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+MAX_BUF_SIZE = 512
 
+
+def readall(connection: socket.socket):
+    """
+    This readall method needs to handle cases when not all bytes are read (edge cases).
+    """
+    return connection.recv(MAX_BUF_SIZE)
+
+
+def handle_connection(connection: socket.socket):
+    while True:
+        buf = connection.recv(MAX_BUF_SIZE)
+        if not buf:  # empty buffer means client has disconnected
+            break
+
+        connection.sendall(b"+PONG\r\n")
+
+
+def main():
+    print("Logs from your program will appear here!")
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, _ = server_socket.accept() # wait for client
-    connection.sendall(b"+PONG\r\n")
+
+    while True:
+        connection, _ = server_socket.accept()  # wait for client
+        handle_connection(connection)
+
 
 if __name__ == "__main__":
     main()
