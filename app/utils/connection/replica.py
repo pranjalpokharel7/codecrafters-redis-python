3f97a45fcdaadb883a04fa21bbb71d8e98133656
pranjalpokharel7 <pranjalpokharel007@gmail.com>
2025-07-21 16:57:58 +0545

    codecrafters submit [skip ci]

--- app/utils/connection/replica.py
+++ app/utils/connection/replica.py
@@ -26,7 +26,7 @@ def connect_to_master_replica(
         # all propagated commands from master will be listened to by this thread
         threading.Thread(
             target=handle_connection,
-            args=(replica.sock, execution_context, replica.buf, False),
+            args=(replica.sock, execution_context, replica.buf, True),
         ).start()
 
     except Exception as e:
