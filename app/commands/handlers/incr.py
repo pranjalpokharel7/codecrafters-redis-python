024a90211f267317090fea4ec707bee87e79f200
pranjalpokharel7 <pranjalpokharel007@gmail.com>
2025-07-22 22:14:56 +0545

    codecrafters submit [skip ci]

--- app/commands/handlers/incr.py
+++ app/commands/handlers/incr.py
@@ -1,9 +1,9 @@
 from app.commands.base import ExecutionContext, ExecutionResult, RedisCommand
-from app.commands.parser import CommandArgParser
 from app.commands.handlers import CommandSet
+from app.commands.parser import CommandArgParser
+from app.resp.types import Integer
 from app.resp.types.simple_error import SimpleError
 from app.storage.in_memory.errors import KeyDoesNotExist, KeyExpired
-from app.resp.types import Integer
 from app.storage.types import RedisValue
 
 
@@ -19,11 +19,6 @@ class CommandIncr(RedisCommand):
     args: dict
     write: bool = True
 
-    def _incr_value(self, value: RedisValue) -> RedisValue:
-        incr = int(value.raw_bytes.decode()) + 1
-        value.raw_bytes = bytes(incr)
-        return value
-
     def __init__(self, args_list: list[bytes]):
         parser = CommandArgParser()
         parser.add_argument("key", 0)
@@ -32,13 +27,19 @@ class CommandIncr(RedisCommand):
     def exec(self, ctx: ExecutionContext, **kwargs) -> ExecutionResult:
         key = self.args["key"]
         try:
-            value = ctx.storage.update(key, self._incr_value)
+            value = ctx.storage.update(key, _incr_value)
             return bytes(Integer(bytes(value)))
 
         except (KeyDoesNotExist, KeyExpired):
             # create a new key with integer value 1
             CommandSet([key, b"1"]).exec(ctx)
             return bytes(Integer(b"1"))
-        
+
         except (ValueError, UnicodeDecodeError):
             return bytes(SimpleError(b"ERR value is not an integer or out of range"))
+
+
+def _incr_value(value: RedisValue) -> RedisValue:
+    incr = int(value.raw_bytes.decode()) + 1
+    value.raw_bytes = str(incr).encode()
+    return value
